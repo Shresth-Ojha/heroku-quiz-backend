98a26c0c5a0eefd087280224819466104618ac64
Shresth-Ojha <shresthojha.iitdelhi@gmail.com>
2024-01-31 06:08:40 +0530

    cors enabled for all

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
 const user_1 = __importDefault(require("./routes/user"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const quiz_1 = __importDefault(require("./routes/quiz"));
 const exam_1 = __importDefault(require("./routes/exam"));
 const report_1 = __importDefault(require("./routes/report"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || '';
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send("Backend for quiz-app");
+    res.send("hey, this is backend lol");
 })
 //redirect /user requests to userRoute
 app.use('/user', user_1.default);
